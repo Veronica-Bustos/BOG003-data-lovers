bbdbd82ed235dce6fa35ff0c951247ad320fc55a
Marieth Verónica Bustos <veronica_9431@outlook.com>
2021-08-31 08:28:45 -0500

    TESTING REDUCE

--- test/data.spec.js
+++ test/data.spec.js
@@ -1,4 +1,4 @@
-import { filtrar, ordenar } from "../src/data.js";
+import { filtrar, ordenar, contarStatus } from "../src/data.js";
 
 describe("test filtrar", () => {
     it("es una funcion", () => {
@@ -25,11 +25,15 @@ describe("test filtrar", () => {
     ];
 
     it("devuelve los personajes filtrados (Male)", () => {
-        expect(filtrar(personajesSinFiltrar,"Male")).toEqual(personajesFiltradosMales);
+        expect(filtrar(personajesSinFiltrar, "Male")).toEqual(
+            personajesFiltradosMales
+        );
     });
 
     it("devuelve los personajes filtrados (Female)", () => {
-        expect(filtrar(personajesSinFiltrar,"Female")).toEqual(personajesFiltradosFemales);
+        expect(filtrar(personajesSinFiltrar, "Female")).toEqual(
+            personajesFiltradosFemales
+        );
     });
 });
 
@@ -52,5 +56,50 @@ describe("ordenar", () => {
 
     it("devuelve los personjes ordenados", () => {
         expect(ordenar(personajesprueba, "a-z")).toEqual(personajesordenados);
+    });
+});
+
+describe("contar status (reduce)", () => {
+    it("es una funcion", () => {
+        expect(typeof contarStatus).toBe("function");
+    });
+
+    const personajes = [
+        {
+            name: "Rick Sanchez",
+            status: "Alive",
+        },
+        {
+            name: "Morty Smith",
+            status: "Alive",
+        },
+        {
+            name: "Summer Smith",
+            status: "Alive",
+        },
+        {
+            name: "Beth Smith",
+            status: "Alive",
+        },
+        {
+            name: "Jerry Smith",
+            status: "Alive",
+        },
+        {
+            name: "Adjudicator Rick",
+            status: "Dead",
+        },
+        {
+            name: "Agency Director",
+            status: "Dead",
+        },
+        {
+            name: "Alan Rails",
+            status: "Dead",
+        },
+    ];
+
+    it("cuenta los personajes de un status", () => {
+        expect(contarStatus(personajes, "Dead")).toEqual(3);
     });
 });
